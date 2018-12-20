54654482cb014b34003b96ecf79279d3bd8cba46
Bhanu Ailani <bhanu.ailani22@gmail.com>
2018-12-20 17:40:32 +0530

    Fizzbuzz problem (#9)
    * problem fizzbuzz
    
    * fizzbuzz in c++
    
    * Update Ide.cpp

---
+++ Day1/C++/Ide.cpp
@@ -0,0 +1,27 @@
+/**
+ * @author: Bhanu0202
+ * @date: 20-12-2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	for(int i = 1; i < n; i++){
+	    if( i % 15 == 0){
+	        cout << "fizzbuzz, ";
+	    }
+	    else if( i % 3 == 0){
+	        cout << "fizz, ";
+	    }
+	    else if( i % 5 == 0){
+	        cout << "buzz, ";
+	    }
+	    else
+	    cout << i << ", ";
+	}
+	return 0;
+}
