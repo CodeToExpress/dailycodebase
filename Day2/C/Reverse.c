fae5edfca0c289f186e57ebe62415c285aa23f5a
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-12-21 19:39:00 +0530

    Create Reverse.c

---
+++ Day2/C/Reverse.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+void reverse(char Str[]){
+    char temp;
+    int i;
+
+    for(i=0; i<(strlen(Str)/2); i++){
+        temp = Str[i];
+        Str[i] = Str[strlen(Str)-i-1];
+        Str[strlen(Str)-i-1] = temp;
+    }
+}
+
+void main(){
+
+    char Str[50], Rev[50];
+    printf("Enter a string = ");
+    scanf("%s", Str);
+
+    strcpy(Rev, Str);
+    reverse(Rev);
+    
+    printf("Reverse = %s", Rev);
+}
