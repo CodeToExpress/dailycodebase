f589e8ef27eb558ce54f261aec83f2c0c0b0dbb3
Utkarsh Malik <utkarshmalik06@gmail.com>
2018-12-22 13:25:42 +0530

    day1

---
+++ Day1/C++/utkarshmalik.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+
+  for(int i=1;i<=n;i++)
+  {
+    if(i%15==0)
+    {cout<<"FizzBuzz ";}
+
+    else if(i%3==0)
+    {cout<<"Fizz ";}
+
+    else if(i%5==0)
+    {cout<<"Buzz ";}
+    
+    else
+    {cout<<i<<" ";}
+    
+  }
+
+}
