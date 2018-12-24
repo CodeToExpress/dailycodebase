93375b02eb68b360e4a71f3552fbbd5156e6888f
moulikcipherX <qwertymoulik@gmail.com>
2018-12-25 01:39:41 +0530

    Day 3 Problem

---
+++ Day3/C++/00x3.cpp
@@ -0,0 +1,36 @@
+//g++  5.4.0
+// Moulik Aggarwal - www.github.com/moulikcipherx
+
+//all header file included
+#include <bits/stdc++.h>
+
+//for STL library
+using namespace std;
+
+//humming distance of two strings
+int humming(string str1, string str2)
+{
+    int count = 0;
+    for(int i = 0; i< str1.length(); i++)
+    {
+        if(str1[i] != str2[i]) count++;        //Increment the counter when char is not match
+    }
+    return count;
+}
+
+//driver function
+int main()
+{
+    string str1,str2;
+
+    //Enter the two strings
+
+    std::cout<<"Enter the first String\n";
+    std::cin>>str1;
+
+    std::cout<<"Enter the Second String\n";
+    std::cin>>str2;
+
+    std::cout<<"Humming Distance of Given Strings is: "<<humming(str1,str2)<<'\n';
+    return 0;
+}
