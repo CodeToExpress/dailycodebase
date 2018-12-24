11e3f275af481d23a24380fa365684bd2f5e468b
Udit Singhania <30744845+udisinghania@users.noreply.github.com>
2018-12-24 16:56:10 +0530

    Update hamming.py

--- Day3/Python/hamming.py
+++ Day3/Python/hamming.py
@@ -1,9 +1,14 @@
-#udisinghania
-#24-12-18
+"""
+  @author : udisinghania
+  @date : 24/12/2018
+"""
 string1 = input()
 string2 = input()
 a = 0
-for i in range (len(string1)):
-    if string1[i]!=string2[i]:
-        a+=1
-print(a)
+if len(string1)!=len(string2):
+    print("Strings are of different length")
+else:    
+    for i in range (len(string1)):
+        if string1[i]!=string2[i]:
+            a+=1
+    print(a)
