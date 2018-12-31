26f256c9273e29e5fd1cff155bfaa7a2b3f3f6c3
Aadit Rahul Kamat <aadit.k12@gmail.com>
2018-12-31 16:38:09 +0800

    Add Day 8 solution in Java

--- day8/Java/LevenshteinDistance.java
+++ day8/Java/LevenshteinDistance.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 class LevenshteinDistance {
-    public static int ind(int m, int n) {
-        return m == n;
+    public static int ind(char m, char n) {
+        return m == n ? 0 : 1;
     }
 
     public static int dist(String first, String second) {
         int firstLength = first.length(), secondLength = second.length();
-        int m[firstLength + 1][secondLength + 1];
+        int[][] m = new int[firstLength + 1][secondLength + 1];
         for (int i = 1; i <= firstLength; i++) {
             m[i][0] = i;
         }
@@ -16,7 +17,7 @@ class LevenshteinDistance {
         }
         for (int i = 1; i <= firstLength; i++) {
             for (int j = 1; j <= secondLength; j++) {
-                int[] values = { m[i - 1][j] + 1, m[i][j - 1] + 1, m[i - 1][j - 1] + ind(first[i], second[j]) };
+                int[] values = { m[i - 1][j] + 1, m[i][j - 1] + 1, m[i - 1][j - 1] + ind(first.charAt(i - 1), second.charAt(j - 1)) };
                 Arrays.sort(values);
                 m[i][j] = values[0];
             }
@@ -29,7 +30,7 @@ class LevenshteinDistance {
         System.out.println("Enter two strings: ");
         first = input.next();
         second = input.next();
-        System.out.printf("The Levenshtein distance between \"%s\" and \"%s\" is: %i\n", first, second, dist(first, second));
+        System.out.printf("The Levenshtein distance between \"%s\" and \"%s\" is: %d\n", first, second, dist(first, second));
     }
 }
 
