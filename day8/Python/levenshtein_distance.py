c4c0f9d0b9e3c82287f113d6e107785c2655f5a9
Aadit Rahul Kamat <aadit.k12@gmail.com>
2018-12-31 17:35:12 +0800

    Update Python implementation

--- day8/Python/levenshtein_distance.py
+++ day8/Python/levenshtein_distance.py
@@ -14,12 +14,12 @@ def dist(first, second):
     m = []
     for i in range(first_length + 1):
         row = []
-        for j in range(first_length + 1):
+        for j in range(second_length + 1):
            row.append(0)
         m.append(row)
 
-    for i in range(first_length + 1):
-        m[i].append(i);
+    for i in range(1, first_length + 1):
+        m[i][0] = i;
 
     for i in range(second_length + 1):
         m[0][i] = i;
@@ -29,6 +29,8 @@ def dist(first, second):
             values = [m[i - 1][j] + 1, m[i][j - 1] + 1, m[i - 1][j - 1] + ind(first[i - 1], second[j - 1])]
             values.sort()
             m[i][j] = values[0]
+
+
     return m[first_length][second_length]
 
 def main():
