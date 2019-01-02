3c13c765091ba588157b3d717f35babc05016b97
KarthiKeyan <karthikeyan90422@gmail.com>
2019-01-02 21:57:42 +0530

    Geekykarthik (#105)
    * Add geekykarthik as a contributor
    
    * Add geekykarthik as a contributor

--- day10/JavaScript/sol.js
+++ day10/JavaScript/sol.js
@@ -1,1 +1,27 @@
-// To Be Added
+/*
+* @author : Karthikeyan
+* @date : 02/01/2019
+*/
+
+function permut(string) {
+    if (string.length < 2) return string; // This is our break condition
+
+    var permutations = []; // This array will hold our permutations
+
+    for (var i=0; i<string.length; i++) {
+        var char = string[i];
+
+        // Cause we don't want any duplicates:
+        if (string.indexOf(char) != i) // if char was used already
+            continue;           // skip it this time
+
+        var remainingString = string.slice(0,i) + string.slice(i+1,string.length); //Note: you can concat Strings via '+' in JS
+
+        for (var subPermutation of permut(remainingString))
+            permutations.push(char + subPermutation)
+
+    }
+    return permutations;
+}
+
+permut('123');
