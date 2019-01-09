8c188b133162068a94dfab7a49f105a92ff34350
MadhavBahlMD <madhavbahl10@gmail.com>
2019-01-09 11:58:38 +0530

    Add day 15

---
+++ day15/JavaScript/pascal_MadhavBahlMD.js
@@ -0,0 +1,28 @@
+/**
+ * @author MadhavBahlMD
+ * @date 09/01/2018
+ */
+
+function findPascal (row, col) {
+    if (col === 1 || col === row) {
+        return 1;
+    } else {
+        return findPascal (row-1, col-1) + findPascal (row-1, col);
+    }
+}
+
+function printPascal (num) {
+    let currentRow;
+    for (let i=1; i<=num; i++) {
+        currentRow = '';
+        for (let j=1; j<=i; j++) {
+            currentRow += findPascal (i, j) + ' ';
+        }
+        console.log(currentRow);
+    }    
+}
+
+console.log ('/* ===== Pascal\'s Triangle for n = 5\n');
+printPascal (5);
+console.log ('\n/* ===== Pascal\'s Triangle for n = 7\n');
+printPascal (7);
