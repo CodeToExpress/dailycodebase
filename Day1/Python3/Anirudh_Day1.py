837ef9f24f9034cca4d616c11425db3bbf674dfa
anirudh-jwala <anirudhsai@gmail.com>
2019-01-12 10:35:18 +0530

    Day1 Commit

---
+++ Day1/Python3/Anirudh_Day1.py
@@ -0,0 +1,11 @@
+print("Enter the value of n: ")
+n = int(input())
+for i in range(1, n+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Fizz Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
