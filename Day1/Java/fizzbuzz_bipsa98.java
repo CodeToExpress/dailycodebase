7c01560f4f9954fa77483aa1fb054ce8094b9a94
MADHAV BAHL <madhavbahl10@gmail.com>
2019-01-14 13:24:21 +0530

    Update fizzbuzz_bipsa98.java

--- Day1/Java/fizzbuzz_bipsa98.java
+++ Day1/Java/fizzbuzz_bipsa98.java
@@ -11,16 +11,13 @@ public class FizzBuzz{
        for(i=1;i<=n;i++)
        {
            if (i%3 == 0 && i%5 == 0)
-           System.out.println("FizzBuzz");
+               System.out.println("FizzBuzz");
            else if(i%3 == 0)
-           System.out.println("Fizz");
-           
+               System.out.println("Fizz");
            else if (i%5 == 0)
-           System.out.println("Buzz");
+               System.out.println("Buzz");
            else
-           System.out.println(i);
-           
-           
+               System.out.println(i);
        }
      }
 }
