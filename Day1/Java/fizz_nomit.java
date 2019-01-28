9faa679c3ef48cd1ed7e4328da5ae5fd303ab701
Nomit Pahuja <pahujanomit@gmail.com>
2019-01-28 22:33:04 +0530

    Day1 Complete (#197)

---
+++ Day1/Java/fizz_nomit.java
@@ -0,0 +1,24 @@
+/*
+@author Nomit Pahuja	
+date: 27/01/19
+*/ 
+import java.util.Scanner;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the value of n: ");
+		int n = input.nextInt();
+		for(int i=1; i<n+1; i++) {
+			if(i%3 == 0 && i%5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if(i%3  == 0) {
+				System.out.println("Fizz");
+			} else if(i%5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
