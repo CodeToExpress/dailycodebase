15a287a98bf9c8d4994734843ac65c0c3c1ba943
Anil Khatri <anil.soccer.khatri@gmail.com>
2019-02-01 11:06:44 +0530

    Month End

--- day30/C++/naiveSearch.cpp
+++ day30/C++/naiveSearch.cpp
@@ -1,36 +1,3 @@
-![cover](./cover.png)
-
-# Day 30 - Search and Sort Algorithms Part C: Naive Pattern Search
-
-So this week is for searching and sorting algorithms, and we did linear search and binary search yesterday, and today's algorithm would be the Naive Search.
-
-## Question
-
-Given a sentence (string), and a pattern, write a function that returns the index of found pattern. Since it is used for pattern searching, after doing it also have a look at KMP algorithm for pattern searching (We already did the KMP algorithm while string problems series, but since today's topic is searching, it deserves a place here as well 😁)
-
-**Example**
-
-```
-input:
-  str = "Hello World, Coding is beautiful"
-  pattern = "World"
-
-output: 6 (start index of the found pattern)
-```
-
-![ques](./ques.png)
-
-## Solution
-
-### [JavaScript Implementation](./JavaScript/binary.js)
-
-```js
-to be added
-```
-
-### [C++ Implementation](./C++/naiveSearch.cpp)
-
-```cpp
 /*
 * @author : imkaka
 * @date   : 1/2/2019
@@ -109,4 +76,3 @@ int main(){
     KMPsearch(txt, pat);
     return 0;
 }
-```
