a2def3e40f8ddfed475064ab736abf28b5138d4e
Anil Khatri <anil.soccer.khatri@gmail.com>
2019-02-05 16:46:56 +0530

    Merge Sort

--- day34/C++/mergeSort.cpp
+++ day34/C++/mergeSort.cpp
@@ -1,35 +1,3 @@
-![cover](./cover.png)
-
-# Day 34 - Search and Sort Algorithms Part G: Merge Sort
-
-Now that we have come this far in searching and sorting algorithms, let's look at a few more algorithms for a couple of days and then we will proceed with questions on data structures like linked lists (singly linked and doubly linked), stacks, queues, trees etc. So today, try to implement merge sort.
-
-Referance: https://www.geeksforgeeks.org/merge-sort/
-
-## Question
-
-**Type:** Divide and Conquer
-
-Given an unsorted list of elements, write a program to sort the given list using merge sort.
-
-**Example**
-
-```
-input: [1, 5, 2, 7, 3, 4, 8, 9, 6]
-output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-```
-
-## Solution
-
-### [JavaScript Implementation](./JavaScript/mergeSort.js)
-
-```js
-to be added
-```
-
-### [C++ Implementation](./C++/mergeSort.cpp)
-
-```cpp
 /*
 * @author : imkaka
 * @date   : 5/2/2019
@@ -124,4 +92,3 @@ int main(){
     print(arr);
     return 0;
 }
-```
