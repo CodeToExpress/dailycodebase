beb47d9565171dfe5adb08be73c82b2dbf5ea07c
Aadit Kamat <aadit.k12@gmail.com>
2019-02-17 11:22:52 +0800

    Add C++ solution for day 42 problem

---
+++ day42/C++/alternate_queue.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<queue>
+#define SIZE 4
+int main() {
+  std::queue<int> first, second, result;
+  int firstElements[SIZE] = {4, 3, 2, 1};
+  int secondElements[SIZE] = {8, 7, 6, 5};
+  for (int i = 0; i < SIZE; i++) {
+    first.push(firstElements[i]);
+    second.push(secondElements[i]);
+  }
+  while(!first.empty() && !second.empty()) {
+    result.push(second.front());
+    second.pop();
+    result.push(first.front());
+    first.pop();
+  }
+  while (!result.empty()) {
+    std::cout << result.front() << " ";
+    result.pop();
+  }
+  std::cout << "\n";
+  return 0;
+}
