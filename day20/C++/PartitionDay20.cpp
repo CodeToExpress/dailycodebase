ed14a283364acf29031994fa611610d37995e53f
ARsh-ALgo <72185264+ARsh-ALgo@users.noreply.github.com>
2020-10-10 18:55:44 +0530

    Update PartitionDay20.cpp

--- day20/C++/PartitionDay20.cpp
+++ day20/C++/PartitionDay20.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main(){
 	int n;
-	cout << "Enter the number of values you want to have."<<endl;
+	cout << "Enter the size of the original array: "<<endl;
 	cin >> n;
 	int a[n];
 	for(int i = 0; i < n; i++){
