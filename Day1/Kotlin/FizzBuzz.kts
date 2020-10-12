df64bbed4a8a34ff6905e5e3060c58f607868236
d-l-mcbride <dmcbride@dmcbride.local>
2020-10-11 20:00:02 -0700

    Add Kotlin solution for FizzBuzz

---
+++ Day1/Kotlin/FizzBuzz.kts
@@ -0,0 +1,21 @@
+/**
+ * @author: d-l-mcbride
+ * @date: 10/11/2020
+ **/
+
+fun fizzBuzz (fizzList: List<Int>) {
+
+    var output: String
+
+    fizzList.forEach {
+        output = when {
+            it % 15 == 0 -> "FizzBuzz"
+            it % 5 == 0 -> "Buzz"
+            it % 3 == 0 -> "Fizz"
+            else -> "${it}"
+        }
+        println(output)
+    }
+}
+
+fizzBuzz((1..15).toList())
