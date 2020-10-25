47f888981435caae4b9f29b37acaa82b4639144c
Ayush Goyal <73097046+NormalVad@users.noreply.github.com>
2020-10-25 21:12:33 +0530

    Python
    Linear Search program in python programming language

---
+++ day28/LinearSearch.py
@@ -0,0 +1,20 @@
+def LinearSearch(a,n):
+    l = len(a)
+    found = False
+    i = 0
+    while(i<l):
+        if(a[i]==n):
+            found = True
+            break
+        else:
+            i += 1
+    if(found):
+        return i
+    else:
+        return "Undefined"
+
+print("Please input Elements of array in single line seperated by spaces")
+arr = list(map(int, input().split()))
+print("Input element to be found")
+n = int(input())
+print(LinearSearch(arr,n))
