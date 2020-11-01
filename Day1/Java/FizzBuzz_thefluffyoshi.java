0154a08ec84f1379ecb9511ae0c6549aaff6a688
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-11-01 11:06:50 -0800

    added thefluffyoshi Day 1 Fizzbuzz version

--- Day1/Java/FizzBuzz_thefluffyoshi.java
+++ Day1/Java/FizzBuzz_thefluffyoshi.java
@@ -9,25 +9,32 @@ public class FizzBuzz_thefluffyoshi
 {
     public static void main (String[] args)
     {
+        //Variables
         int num, i;
+
+        //Input a num
         System.out.print("Enter a number: ");
         Scanner scan = new Scanner(System.in);
         num = scan.nextInt();
 
+        //Checking inputted num
         for (i = 1; i < num; i++)
         {
+            //Checking if num is divisible by both 5 and 3
             if (i%15 == 0)
             {
                 System.out.print("Fizzbuzz" + " ");
-            }
+            }//end FizzBuzz if
+            //Checking if num is divisible by 5
             else if (i%5 == 0)
             {
                 System.out.print("Buzz" + " ");
-            }
+            }//end Buzz else if
+            //Checking if num is divisible by 3
             else if (i%3 == 0)
             {
                 System.out.print("Fizz" + " ");
-            }
-        }
-    }
-}
+            }//end Fizz if
+        }//end for loop checking
+    }//end main
+}//end class FizzBuzz
