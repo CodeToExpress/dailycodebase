07fb45e838cb8719f91d785f3ae33ab59cb6c133
Parth Patel <65417976+ppm143@users.noreply.github.com>
2020-11-04 21:01:32 +0530

    Added c++code

--- day24/C++/isRotation_parth.cpp
+++ day24/C++/isRotation_parth.cpp
@@ -1,6 +1,6 @@
 /**
  * @author Parth Patel
- * @date 22/09/2020
+ * @date 04/11/2020
  * Method - Circular Search -- Time = O(n)
  */
 
@@ -31,14 +31,16 @@ bool isRotation(int arr1[], int n1, int arr2[], int n2) {
     return true;
 }
 int main() {
-    int n1, n2, arr1[n1], arr2[n2];
+    int n1, n2;
     // Size of array 1 is n1
-    cin >> n1;    
+    cin >> n1;
+    int arr1[n1]
     for (int i = 0; i < n1; i++) {
         cin >> arr1[i];
     }
     // Size of array 2 is n2 
     cin >> n2;
+    int arr2[n2];
     for (int i = 0; i < n2; i++) {
         cin >> arr2[i];
     }
